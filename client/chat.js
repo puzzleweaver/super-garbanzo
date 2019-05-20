d03494f257645c2154771d2dee2411026b7af7b0
Weaver, Austin <austin.weaver@cmegroup.com>
2019-05-20 15:38:26 -0500

    chat

---
+++ client/chat.js
@@ -0,0 +1,25 @@
+
+// var socket = io.connect('localhost:80');
+var socket = io.connect('https://super-garbanzo.herokuapp.com/');
+
+socket.on('msgs', function(data) {
+    document.getElementById('box').innerHTML += data.id + ": " + data.text + "<br><br>";
+});
+
+function start() {
+    socket.emit('start', {
+        id: id,
+    });
+}
+
+var send_msg() {
+  socket.emit("msg", {
+    id:id,
+    text:document.getElementById('text-input').value,
+  });
+}
+
+document.addEventListener('keyup', function(e) {
+    if(e.keyCode == 13) // enter press
+      send_msg();
+});
